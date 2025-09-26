ded22f3f9d56a0ca103e08118c247cb07c060b61
Sathyavit <sathya.m2023@vitstudent.ac.in>
2025-09-26 15:08:47 +0530

    Update Q1.php

--- Assignment 9/Q1.php
+++ Assignment 9/Q1.php
@@ -1,1 +1,30 @@
+<?php
+$students = [
+    "artu" => 85,
+    "kasu" => 92,
+    "Arun"  => 78,
+    "sathya" => 95,
+    "Kiran" => 88,
+    "Sanjay"=> 80
+];
+
+arsort($students);
+$topStudents = array_slice($students, 0, 3, true);
+
+echo "<h3>Top 3 Students</h3>";
+echo "<table border='1' cellpadding='8' cellspacing='0'>";
+echo "<tr><th>Rank</th><th>Name</th><th>Marks</th></tr>";
+
+$rank = 1;
+foreach ($topStudents as $name => $marks) {
+    echo "<tr>";
+    echo "<td>$rank</td>";
+    echo "<td>$name</td>";
+    echo "<td>$marks</td>";
+    echo "</tr>";
+    $rank++;
+}
+
+echo "</table>";
+?>
 
